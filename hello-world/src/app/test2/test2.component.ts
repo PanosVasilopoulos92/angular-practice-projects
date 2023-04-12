0a8ec10a486f13b1b28fcafc2844b844bce2b327
PanosV <panosVasilopoulos92@gmail.com>
2023-04-12 12:26:43 +0300

    added mini projects

---
+++ hello-world/src/app/test2/test2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test2',
+  template: `<div> Inline template </div>`,
+  styleUrls: ['./test2.component.css']
+})
+export class Test2Component {
+
+}
