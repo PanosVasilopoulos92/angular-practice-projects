8130712d59b332fa264973a8c1a4636c748fbbaf
PanosV <panosVasilopoulos92@gmail.com>
2023-04-12 14:03:40 +0300

    some updates

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -2,9 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `<div>
+    <h2>Welcome {{name}}</h2>
+    <h2>{{ 2 + 2 }}</h2>
+    <h2>{{ 'Welcome ' + name }}</h2>
+    <h2>{{ 'Name length is ' + name.length + ' letters'}}</h2>
+    <h2>{{ greetUser() }}</h2>
+
+    <h1>Property binding</h1>
+    <input [id]= "myId" type="text" value="Type here">
+    <input [disabled]="isDisabled" id="{{ myId }}" type="text" value="Type here">
+    <input bind-disabled="isDisabled" id="{{ myId }}" type="text" value="Type here">
+  </div>`,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
+  // name: string = 'Panos'
+  public name = 'Panos';
+  public myId = 'testId';
+  public isDisabled = false;
+  constructor() {}
 
+  greetUser() {
+    return 'Hello ' + this.name;
+  }
 }
